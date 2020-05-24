ad520c6b6285ee2e7fab42ccd54d658f1cc94fcd
hienP <freedom9x@gmail.com>
2020-05-24 12:15:40 +0700

    Updates

---
+++ precache-manifest.2732a372560c13cb6ba66602788d0885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ba616ec272433eaf7c67e5188f17ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd4dfc80d98c3e120a5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f27b23dbfbda03f917d",
+    "url": "/static/js/2.628677f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.628677f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd4dfc80d98c3e120a5",
+    "url": "/static/js/main.8fee2bbd.chunk.js"
+  },
+  {
+    "revision": "42b1de5745ea97aba104",
+    "url": "/static/js/runtime-main.08d8c977.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
